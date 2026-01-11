f8aa13b9ca4e34eb432963d340ab268b19121353
David Padilla <davidpaag2002@gmail.com>
2026-01-11 23:52:39 +0100

    feat: Added canvas and first styles

---
+++ index.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d", { willReadFrequently: true });
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
